56246bc241a287e0d28e0207c5fc1d7d7b1128d8
Hadi Hammoud <hhadi@umich.edu>
2024-04-14 00:06:10 -0400

    Add tests for mixed numbers and duplicates in SelectionSort

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -38,10 +38,46 @@ public void testNegative(){
 	/** Test data contains negative values only **/
 }
 public void testMixed(){
-		/** Test data contains with both positive, negative and zeros **/		
-}
-public void testDuplicates(){
-		/** Test data contains duplicates **/		
-}
+		// TODO Auto-generated method stub
+		int[] arr = new int[5];
+		arr[0] = -8;
+		arr[1] = 9;
+		arr[2] = -7;
+		arr[3] = 10;
+		arr[4] = -2;
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = 2;
+		Sortedarr[1] = -7;
+		Sortedarr[2] = 8;
+		Sortedarr[3] = -9;
+		Sortedarr[4] = 10;
+		/** Test data contains with both positive, negative and zeros **/
+		SelectionSort selectionSort = new SelectionSort();
+	     assertArrayEquals(Sortedarr, selectionSort.basicSelectionSort(arr));
+		
+	}
+	public void testDuplicates(){
+		// TODO Auto-generated method stub
+		int[] arr = new int[5];
+		arr[0] = 8;
+		arr[1] = 9;
+		arr[2] = 8;
+		arr[3] = 9;
+		arr[4] = 2;
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = 2;
+		Sortedarr[1] = 7;
+		Sortedarr[2] = 8;
+		Sortedarr[3] = 7;
+		Sortedarr[4] = 2;
+		/** Test data contains duplicates **/
+		SelectionSort selectionSort = new SelectionSort();
+	     assertArrayEquals(Sortedarr, selectionSort.basicSelectionSort(arr));
+		
+	}
+	
+	private void assertArrayEquals(int[] sortedarr, int[] basicSelectionSort) {
+		// TODO Auto-generated method stub
+	}
 	
 }
