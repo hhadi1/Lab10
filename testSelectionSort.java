9bab24e9ebcf48c508a1563247972b00a1a2d208
Hadi Hammoud <hhadi@umich.edu>
2024-04-14 00:05:04 -0400

    Add tests for positive and negative numbers in SelectionSort

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -19,24 +19,46 @@ public class testSelectionSort {
 }
 public testSelectionSort() {
 }
-public void testPositive(){
-	int[] arr = new int[5];
-	arr[0] = 8;
-	arr[1] = 9;
-	arr[2] = 7;
-	arr[3] = 10;
-	arr[4] = 2;
-	int[] Sortedarr = new int[5];
-	Sortedarr[0] = 2;
-	Sortedarr[1] = 7;
-	Sortedarr[2] = 8;
-	Sortedarr[3] = 9;
-	Sortedarr[4] = 10;
-	/** add tests to check for this unit test **/
-}
-public void testNegative(){
-	/** Test data contains negative values only **/
-}
+    @Test
+	public void testPositive(){
+    	// TODO Auto-generated method stub
+		int[] arr = new int[5];
+		arr[0] = 8;
+		arr[1] = 9;
+		arr[2] = 7;
+		arr[3] = 10;
+		arr[4] = 2;
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = 2;
+		Sortedarr[1] = 7;
+		Sortedarr[2] = 8;
+		Sortedarr[3] = 9;
+		Sortedarr[4] = 10;
+		/** add tests to check for this unit test **/
+		 SelectionSort selectionSort = new SelectionSort();
+	     assertArrayEquals(Sortedarr, selectionSort.basicSelectionSort(arr));
+	}
+    
+    @Test
+	public void testNegative(){
+    	// TODO Auto-generated method stub
+    	int[] arr = new int[5];
+		arr[0] = -8;
+		arr[1] = -9;
+		arr[2] = -7;
+		arr[3] = -10;
+		arr[4] = -2;
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = -2;
+		Sortedarr[1] = -7;
+		Sortedarr[2] = -8;
+		Sortedarr[3] = -9;
+		Sortedarr[4] = -10;
+		/** Test data contains negative values only **/
+    	SelectionSort selectionSort = new SelectionSort();
+	     assertArrayEquals(Sortedarr, selectionSort.basicSelectionSort(arr));
+		
+	}
 public void testMixed(){
 		/** Test data contains with both positive, negative and zeros **/		
 }
